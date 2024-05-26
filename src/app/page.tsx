8f109a71db13a3dd1898f5d0376de5b1c68671e5
MidnightYKT <nomiama_ykt@mail.ru>
2024-05-25 18:08:41 -0700

    added API post and created components Product

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 
+import Product from "@/components/Products/Product";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import type { Product as TProduct } from "@/db";
 import { cn } from "@/lib/utils";
+import { useQuery } from "@tanstack/react-query";
+import { QueryResult } from "@upstash/vector";
+import axios from "axios";
 import { ChevronDown, Filter } from "lucide-react";
-import PreviousMap from "postcss/lib/previous-map";
 import { useState } from "react";
 
 const SORT_OPTIONS = [
@@ -21,7 +25,23 @@ export default function Home() {
     sort: "none",
   });
 
-  console.log(filter);
+  const { data: products } = useQuery({
+    queryKey: ["products"],
+    queryFn: async () => {
+      const { data } = await axios.post<QueryResult<TProduct>[]>(
+        "http://localhost:3000/api/products",
+        {
+          filter: {
+            sort: filter.sort,
+          },
+        }
+      );
+
+      return data;
+    },
+  });
+
+  console.log(products);
 
   return (
     <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -63,6 +83,19 @@ export default function Home() {
           </button>
         </div>
       </div>
+
+      <section className="pb-24 pt-6">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
+          {}
+          <div></div>
+
+          <ul className="lg:col-span-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+            {products?.map((product) => (
+              <Product key={product.id} product={product.metadata!} />
+            ))}
+          </ul>
+        </div>
+      </section>
     </main>
   );
 }
